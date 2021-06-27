444878e71b2a1f180576cf144c236f2e3d6c9a15
Daniël R <danielroth1890@gmail.com>
2021-06-27 15:10:48 +0200

    finish day 1 part 2

--- Dag1/Dag1.js
+++ Dag1/Dag1.js
@@ -2,8 +2,7 @@ import { readFileSync } from 'fs';
 
 var data = readFileSync('Dag1.txt', 'utf8').split('\n');
 var ansPart1 = 0;
-
-console.log(CalcAll(data, ansPart1));
+var ansPart2 = 0;
 
 // Functie om nodige benzine voor één onderdeel te berekenen
 function Calc(getal) {
@@ -18,3 +17,24 @@ function CalcAll(data, ans) {
   });
   return ans;
 }
+
+// Functie om nodige benzine te berekenen per onderdeel en daarna benzine te berekenen per benzine capsule zolang de benzine die nodig is groter dan 0 is
+function CalcModule(getal, ans) {
+  while (Calc(getal) > 0) {
+    getal = Calc(getal);
+    ans += getal;
+  }
+
+  return ans;
+}
+
+// Functie om nodige benzine en benzine voor de benzine te berekenen voor alle onderdelen
+function CalcModules(data, ans) {
+  data.forEach((getal) => {
+    ans += CalcModule(getal, 0);
+  });
+  return ans;
+}
+
+console.log('Antwoord deel 1:', CalcAll(data, ansPart1));
+console.log('Antwoord deel 2', CalcModules(data, ansPart2));
