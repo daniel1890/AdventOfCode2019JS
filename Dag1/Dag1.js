b28f95e0d7149d2338889cab428f4936a5808261
Daniël R <danielroth1890@gmail.com>
2021-06-27 04:12:58 +0200

    Dag 1 part 1 completed

--- Dag1/Dag1.js
+++ Dag1/Dag1.js
@@ -1,16 +1,20 @@
 import { readFileSync } from 'fs';
 
 var data = readFileSync('Dag1.txt', 'utf8').split('\n');
+var ansPart1 = 0;
 
-console.log(data);
+console.log(CalcAll(data, ansPart1));
 
+// Functie om nodige benzine voor één onderdeel te berekenen
 function Calc(getal) {
   getal = Math.floor(getal / 3) - 2;
   return getal;
 }
 
-console.log(
+// Functie om nodige benzine voor alle onderdelen in data te berekenen en het antwoord teruggeeft.
+function CalcAll(data, ans) {
   data.forEach((getal) => {
-    Calc(getal);
-  })
-);
+    ans += Calc(getal);
+  });
+  return ans;
+}
