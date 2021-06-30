abacee340b1b79ffdd6de49997d04bc9d9c3c58f
Daniël R <danielroth1890@gmail.com>
2021-06-30 18:08:49 +0200

    Finish day 2, need to reformat some still

--- Dag2/Dag2.js
+++ Dag2/Dag2.js
@@ -1,17 +1,32 @@
 import { readFileSync } from 'fs';
 
-var data = readFileSync('Dag2.txt', 'utf8').split(',');
+let data;
+let intCodeLog = {};
+let ansPart2 = {};
 const test_data = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
 
-function IntCodeCompiler(data) {
-  var stap = 4;
-  var opcode = 0;
+const resetData = (data, location) => {
+  data = [];
+  data = readFileSync(location, 'utf8').split(',');
+  return data;
+};
+
+const IntCodeCompiler = (data, noun, verb) => {
+  let stap = 4;
+  let opcode = 0;
+  let output = data[0];
+  data[1] = noun;
+  data[2] = verb;
 
   while (data[opcode] != 99) {
-    var input1 = data[opcode + 1];
-    var input2 = data[opcode + 2];
-    var output = data[opcode + 3];
+    let input1 = data[opcode + 1];
+    let input2 = data[opcode + 2];
+    let output = data[opcode + 3];
 
+    if (parseInt(data[0]) == 19690720) {
+      intCodeLog = 100 * parseInt(data[1]) + parseInt(data[0]);
+      break;
+    }
     if (data[opcode] == 1) {
       data[output] = parseInt(data[input1]) + parseInt(data[input2]);
     } else if (data[opcode] == 2) {
@@ -21,7 +36,31 @@ function IntCodeCompiler(data) {
     opcode += stap;
   }
 
-  console.log(data);
-}
+  return {
+    output: data[0],
+    noun: noun,
+    verb: verb,
+  };
+};
 
-IntCodeCompiler(data);
+data = resetData(data, 'Dag2.txt');
+
+const searchOutput = (outputToSearch, data) => {
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      intCodeLog = IntCodeCompiler(data, i, j);
+
+      if (intCodeLog.output == outputToSearch) {
+        console.log('FOUND!');
+        return {
+          noun: intCodeLog.noun,
+          verb: intCodeLog.verb,
+        };
+      }
+      data = resetData(data, 'Dag2.txt');
+    }
+  }
+};
+
+ansPart2 = searchOutput(19690720, data);
+console.log(100 * parseInt(ansPart2.noun) + parseInt(ansPart2.verb));
